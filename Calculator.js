aa1864134f8b9edf422111b74d146d97fab3c21d
Danebbd <119659008+Danebbd@users.noreply.github.com>
2022-12-12 16:24:31 +0200

    Add files via upload

--- Calculator.js
+++ Calculator.js
@@ -3,12 +3,12 @@ const calc = document.querySelector(".calc")
 var num1 = '';
 var num2 = '';
 var oper = '';
+var equation = '';
 
 /*Event listner for when page is loaded to make sure js sees all objects*/
 window.addEventListener("load", (event) => {
     /*reads the id of button with click event*/
     let numberButtons = document.getElementsByClassName("numberButton");
-    console.log(numberButtons);
     for (let button of numberButtons){
         button.addEventListener("click",() => printNum(button.id))
     }
@@ -22,13 +22,16 @@ function testError(){/*Tests if the value is Error then clears the variables*/
 
 function printNum(value){/*Used to test if which variable to add the next number and print the values */ 
     if (oper == ''){
-    num1 = num1 + value
+    num1 = num1 + value;
+    equation = equation + value;
     document.getElementById("Textarea").innerHTML = num1;
     } 
     if (oper != ''){
         num2 = num2 + value
+        equation = equation + value;
         document.getElementById("Textarea").innerHTML = num2;
-        } 
+        }
+    document.getElementById("equation") .innerHTML = equation;
 }
 
 function ifNum2exists(){/*Checks for second value and calculates. Resets num2*/
@@ -53,19 +56,24 @@ function calculate(n1,operator,n2){/*gets the two numbers and operator then does
             result = parseFloat(n1) / parseFloat(n2);
         }
       }
+      num1 = result;
       return result;
 }
 /*Buttons*/
 
 function buttonComma() {
     testError();
-    printNum(".")
+    printNum(".");
+    equation = equation + value;
+    document.getElementById("equation") .innerHTML = equation;
 }
 
 function buttonClear() {
     num1 = '';
     num2 = '';
     oper = '';
+    equation = '';
+    document.getElementById("equation") .innerHTML = equation;
     document.getElementById("Textarea").innerHTML= '';
 }
 
@@ -73,29 +81,40 @@ function buttonPlus() {
     testError();
     ifNum2exists();
     oper = 'plus';
+    equation = equation + '+';
     document.getElementById("Textarea").innerHTML = num1;
-  
+    equation = equation + value;
+    document.getElementById("equation") .innerHTML = equation;
 }
 
 function buttonMinus() {
     testError();
     ifNum2exists();
     oper = 'minus';
+    equation = equation + '-';
     document.getElementById("Textarea").innerHTML = num1;
+    equation = equation + value;
+    document.getElementById("equation") .innerHTML = equation;
 }
 
 function buttonTimes(){
     testError();
     ifNum2exists();
     oper='times';
+    equation = equation + '*';
     document.getElementById("Textarea").innerHTML = num1;
+    equation = equation + value;
+    document.getElementById("equation") .innerHTML = equation;
 }
 
 function buttonDivide() {
     testError();
     ifNum2exists();
     oper = 'divide'
+    equation = equation + '/';
     document.getElementById("Textarea").innerHTML = num1;
+    equation = equation + value;
+    document.getElementById("equation") .innerHTML = equation;
 }
 
 function buttonEquals() {
@@ -112,6 +131,10 @@ function buttonEquals() {
     else{
         document.getElementById("Textarea").innerHTML = calculate(num1,oper,num2);
     }
-    num2 = '';   
+    num2 = '';
+    oper = '';
+    equation = equation + '=';
+    document.getElementById("equation") .innerHTML = equation;
+    equation = num1;   
 }
 
